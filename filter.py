98ecdd4453c3d1d49aa26567f0a2842bcca85d9d
Sajib Biswas <45337126+Sajib132@users.noreply.github.com>
2021-02-23 22:16:13 +0530

    Update filter.py

--- filter.py
+++ filter.py
@@ -7,3 +7,7 @@ def is_even(lst):
 lst=[12,13,14,6,17]
 lst1=list(filter(is_even,lst))
 print(lst1)
+# a lambda function that returns even number from a list
+lst=[98,77,43,90]
+lst1=list(filter(lambda x: (x%2==0),lst))
+print(lst1)
